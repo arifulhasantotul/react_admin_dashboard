890d738c393b5e845799ab26717dba87e55322de
arifulhasantotul <mdahtotul@gmail.com>
2022-05-30 10:19:29 +0600

    project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="underline text-3xl">App</h1>
+    </div>
+  );
 };
 
 export default App;
